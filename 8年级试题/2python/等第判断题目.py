a13aef949ea207ec6278d0c0b60554ec6b0e2067
yinggez <49324614+yinggez@users.noreply.github.com>
2022-06-24 00:27:08 +0800

    Update 等第判断题目.py

--- 8年级试题/2python/等第判断题目.py
+++ 8年级试题/2python/等第判断题目.py
@@ -1,10 +1,10 @@
-rank=①(input('请输入你的排名：'))
+pai_ming=①(input('请输入你的排名：'))
 
-if rank<=②:
+if pai_ming<=②:
     print('你的成绩等第为：A')
-elif 10<rank<=25:
+elif 10<pai_ming<=35:
     print('你的成绩等第为：B')
-elif 25<rank<=45:
+elif 35<pai_ming<=50:
     print('你的成绩等第为：C')
 ③:
     print('你的成绩等第为：D')
